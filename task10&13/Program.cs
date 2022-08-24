35facd41cf0b2e4cb2175588a2ce3556131b62db
Mikhail Isupov <miknsk@gmail.com>
2022-08-24 11:05:59 +0700

    ДЗ к семинару 2

---
+++ task10&13/Program.cs
@@ -0,0 +1,28 @@
+﻿// Вводим любое число и любой номер цифры, выводим цифру или сообщаем что ее нет.
+int ExtractNumber (int number, int position) // ищем цифру в числе number на позиции position, возвращаем 0..9 либо -1
+{
+    int slicedNumber = number;
+    int extractedNumber = 0;
+    int i = 0;
+    
+    while ((slicedNumber !=0)&&(i < position))
+    {
+        extractedNumber = slicedNumber % 10;
+        slicedNumber /= 10;
+        i += 1;
+    }
+
+if (i < position) return -1;
+else return extractedNumber;
+}
+
+Console.WriteLine("Введите любое целое число");
+int inputNumber = Math.Abs(int.Parse(Console.ReadLine()));
+
+Console.WriteLine("Введите номер позиции цифры");
+int numberPosition = int.Parse(Console.ReadLine());
+
+int result = ExtractNumber(inputNumber, numberPosition);
+
+if (result == -1) Console.WriteLine("Цифры в позиции " + numberPosition + " не существует");
+else Console.WriteLine("В позиции " + numberPosition + " находится цифра " + result);
---
+++ task15/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вводим номер дня недели и узнаем выходной он или нет
+bool IsHoliday (int number)
+{ 
+    int saturday = 6;
+    int sunday = 7;
+    return (number == saturday)||(number == sunday);
+}
+
+Console.WriteLine("Введите номер дня недели (1..7)");
+int numberOfDay = int.Parse(Console.ReadLine());
+
+if (IsHoliday(numberOfDay)) Console.WriteLine("Это выходной день");
+else Console.WriteLine("Это не выходной день");
